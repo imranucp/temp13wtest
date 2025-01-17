6aa2470ee6e490fdc1281ea7e0e736cc8ca19cbd
imran ashrf <zsaing.ucp@gmail.com>
2025-01-17 14:05:31 +0500

    initial commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+git branch -m develop
+git config --local user.name "imran ashrf"
+git config --local user.email zsaing.ucp@gmail.com
+git config --local core.editor notepad
+touch .gitignore
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css
+npm init -y
